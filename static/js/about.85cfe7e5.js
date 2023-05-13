0d3ac39e297065d083ca52c6025dc89a89fe0902
DDD10Decade <102458086+DDD10Decade@users.noreply.github.com>
2023-05-13 22:55:09 +0800

    聊天界面

--- static/js/about.85cfe7e5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunkfront"]=self["webpackChunkfront"]||[]).push([[443],{7381:function(n,t,r){r.r(t),r.d(t,{default:function(){return l}});var u=r(9003);const a={class:"about"},e=(0,u._)("h1",null,"This is an about page",-1),s=[e];function c(n,t){return(0,u.wg)(),(0,u.iD)("div",a,s)}var o=r(89);const f={},i=(0,o.Z)(f,[["render",c]]);var l=i}}]);
