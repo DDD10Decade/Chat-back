dd1d14f0e828076ad0b6a64d6bb5923b8dc6c3d8
DDD10Decade <102458086+DDD10Decade@users.noreply.github.com>
2023-05-14 16:36:05 +0800

    聊天登入

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from socket import socket
 import time
 from flask import Flask, request, redirect, render_template, url_for
 from flask_socketio import SocketIO, emit
@@ -7,8 +6,6 @@ app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 user_dict = {}
-user_List = []
-client_query = []
 chat_message = {}
 
 
@@ -18,49 +15,47 @@ def login():  # put application's code here
     return render_template("index.html")
 
 
-@app.route("/chat/<username>", methods=["POST", "GET"])
-def chat_main(username):
-    global user_List
-    global user_dict
-    user_dict[str(request.remote_addr)] = username
-    user_List = user_dict.values()
-    emit("update_user", {"online": user_List})
-    print(user_dict)
-
-
 @socketio.on('NewMessage')
 def handle_new_message(data):
     global chat_message
     content = data.get('content')
     name = data.get("name")
     now = time.asctime()
-    chat_message[str(name) + ":" + now] = str(content)
+    chat_message[str(name) + ":" + now] = content.encode("utf-8").decode("utf-8")
     emit('NewMessage', {"content": content, "sendName": name, "sendTime": now}, broadcast=True)
-    print(chat_message)
+
+
+@socketio.on('updateList')
+def update_online_list(new_user):
+    global user_dict
+    name = new_user.get("name")
+    user_dict[str(request.remote_addr) + "/" + name] = name
+    emit("updateList", user_dict, broadcast=True)
+    print(user_dict)
+
+
+@socketio.on("OtherLeave")
+def OtherLeave_update(data):
+    global user_dict
+    name = data.get("name")
+    Leave_key = str(request.remote_addr) + "/" + name
+    emit("OtherLeave", {"name": name})
+    user_dict.pop(Leave_key)
+    print("聊天室现存：")
+    print(user_dict)
 
 
 @socketio.on('connect')  # 有客户端连接会触发该函数
 def on_connect():
-    global user_List
-    global user_dict
-    global client_id
-    # 建立连接 sid:连接对象ID
-    # client_id = request.sid
-    # client_query.append(client_id)
-    # emit(event_name, broadcasted_data, broadcast=True, namespace=name_space)
     # #对name_space下的所有客户端发送消息
-    print(u'new connection,id=[%s] connected, now have [%s] connections' % (request.remote_addr, len(client_query)))
+    print(u'new connection,id=[%s] connected' % request.remote_addr)
 
 
 @socketio.on('disconnect')  # 有客户端断开WebSocket会触发该函数
 def on_disconnect():
-    global user_List
     global user_dict
     # 连接对象关闭 删除对象ID
-    # user_dict.pop(str(request.sid))
-    # client_query.remove(request.sid)
-    # user_List = user_dict.values()
-    print(u'connection id=[%s] exited, now have [%s] connections' % (request.remote_addr, len(client_query)))
+    print(u'connection id=[%s] exited' % request.remote_addr)
 
 
 if __name__ == '__main__':
